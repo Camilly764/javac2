c4da630ccb5749fc6cd1c511e439bbf0c8b4fb37
Guxtaaln <camillylimac04@gmail.com>
2024-09-28 11:58:42 -0300

    Add files via upload

---
+++ javac2/script.js
@@ -0,0 +1,17 @@
+function caracteristicas() {
+ //Buscar interesses
+    let interesses= prompt("Seus interesses");
+//Faixa de preço
+let preço= prompt ("Qual é o seu orçamento");
+//Marcas que gosta
+let marcas= prompt= ("Quais são suas marcas favoritas");
+//Tipos de peças
+let peças= prompt= ("Categorias");
+alert ("Você gostaria de comprar algum produto que seja: "+ interesses +" e qual custo benefício" );
+//Insere preferências
+console.log("Seus interesses:", interesses);
+console.log("Seu orçamento:", preço);
+console.log("Suas marcas favoritas", marcas);
+console.log("Tipos de peças", peças);
+
+}
